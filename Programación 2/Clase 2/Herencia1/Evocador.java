2211090b7da4c2bc1d7006074feb9d777fbc552f
Kirurai <kd.maurii@gmail.com>
2020-04-06 13:44:21 -0300

    Actualizando estadística

---
+++ Programación 2/Clase 2/Herencia1/Evocador.java
@@ -0,0 +1,48 @@
+package Herencia1;
+
+/**
+ *
+ * @author Miño Maurizio <kd.maurii@gmail.com>
+ */
+
+public class Evocador extends Mago{
+    private String habilidad; //TODO: volverlo array para tener un vector de habilidades.
+    private String arquetipo;
+    private String elemento;
+
+
+    public Evocador(String nombre, int nivel, int fuerza, int inteligencia, String elemento){
+        super(nombre, nivel, fuerza, inteligencia);
+        setHabilidad("Rayo");
+        setArquetipo("Evocador");
+        setElemento(elemento);
+    }
+
+    public String getHabilidad(){
+        return this.habilidad;
+    }    
+    public String getArquetipo() {
+        return this.arquetipo;
+    }
+    public String getElemento() {
+        return this.elemento;
+    }
+
+    public void setHabilidad(String habilidad){
+        this.habilidad = habilidad;
+    }
+    public void setArquetipo(String arquetipo) {
+        this.arquetipo = arquetipo;
+    }
+    public void setElemento(String elemento){
+        this.elemento = elemento;
+    }
+
+    public void presentacion(){
+        System.out.printf("Buenos días! Soy %s y soy un %s de tipo %s! Te despeinaré!", super.getNombre(), this.getArquetipo(), this.getElemento());
+    }
+    public void lanzarHabilidad() {//TODO Modificar cuando se cree el array de habilidades
+        System.out.printf("%s ha convocado un %s desde los cielos!\nHa dejado frito (y despeinado) al enemigo!", super.getNombre(), this.getHabilidad());
+    }
+
+}
